63bf1296090af2e78a4c7e58e3b033c5ef2e936e
Cuong <huucuongyd@gmail.com>
2023-02-15 10:38:22 +0700

    add module car api to appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {DataModule} from './car_database_api/cardb_api.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://NodeJS_Beginning:Anhcuongvp1@cluster0.sdo9noc.mongodb.net/car_store?retryWrites=true&w=majority')],
+  imports: [MongooseModule.forRoot('mongodb+srv://NodeJS_Beginning:Anhcuongvp1@cluster0.sdo9noc.mongodb.net/car_store?retryWrites=true&w=majority'),DataModule],
   controllers: [AppController],
   providers: [AppService],
 })
